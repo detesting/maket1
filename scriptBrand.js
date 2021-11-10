a1c22aa32ad4188a1486c0eeefc3c62490a0cfdd
Anna Topilina <anna-topilina@mail.ru>
2021-11-10 18:38:48 +0300

    test

---
+++ scriptBrand.js
@@ -0,0 +1,15 @@
+new Swiper('.swiper-container', {
+
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },
+
+    // // Стрелки
+    // navigation: {
+    //     nextEl: '.swiper-button-next',
+    //     prevEl: '.swiper-button-prev'
+    // },
+    slidesPerView: 1;
+});
