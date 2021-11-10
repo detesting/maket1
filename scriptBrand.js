18be5893d7d61305d2af2434395ca1ba91283859
Anna Topilina <anna-topilina@mail.ru>
2021-11-10 20:23:00 +0300

    test

--- scriptBrand.js
+++ scriptBrand.js
@@ -1,10 +1,12 @@
 const swiper = new Swiper('.swiper-container', {
+
     pagination: {
         el: '.swiper-pagination',
-        clickable: true
+        type: 'bullets',
+        clickable: true,
     },
     spaceBetween: 16,
-    slidesPerView: 1,
+    slidesPerView: 1.15,
     centeredSlides: true,
     // // Стрелки
     // navigation: {
